c469da92933d53e422e79a824e9c14749ce663b4
Cristina Pena <cristinalpena@gmail.com>
2020-05-24 11:26:23 -0600

    Set up basic structure for Nav, import fonts Co-authored-by: Megan Venetianer <megan.venetianer@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,20 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
-#nav {
-  padding: 30px;
+h1 {
+  font-family: 'Playfair Display', serif;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+h2,
+h3,
+p {
+  font-family: 'Roboto Condensed', sans-serif;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
