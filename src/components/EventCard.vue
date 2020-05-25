d3b196132a24a8f6a90aa6cb5032b153149ab498
Megan Venetianer <megan.venetianer@gmail.com>
2020-05-25 15:30:59 -0600

    Add and style date circle
    Co-authored-by: Cristina Peña <cristinalpena@gmail.com>

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -2,6 +2,9 @@
   <div class="event-card">
     <button class="heart-btn"><img class="unfilled-heart" src="../../public/empty-heart.png" alt="unfilled heart"></button>
     <img src="https://upload.wikimedia.org/wikipedia/commons/1/15/No_image_available_600_x_450.svg" alt="No image available">
+    <div class="event-date">
+      <p>5/22</p>
+    </div>
     <h3>SHAWN MENDES: THE VIRTUAL TOUR</h3>
     <button class="event-details-btn">EVENT DETAILS</button>
   </div>
@@ -38,7 +41,7 @@ img {
 h3 {
   font-size: 24px;
   font-weight: normal;
-  margin-left: 1em;
+  margin: 0 3.2em 0 1em;
 }
 
 .event-details-btn {
@@ -68,6 +71,26 @@ h3 {
   border: none;
   top: -1.7em;
   right: .4em;
+}
+
+.event-date {
+  border-radius: 50%;
+  height: 60px;
+  width: 60px;
+  background: #06d6a0;
+  z-index: 2;
+  position: absolute;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  top: 38%;
+  right: .6em;
+}
+
+p {
+   font-size: 24px;
+   text-align: center;
+   width: 100%;
 }
 
 </style>
