7ab0a58834a390acece082cb56fac95ee291735f
Megan Venetianer <megan.venetianer@gmail.com>
2020-05-25 14:12:45 -0600

    create EventsContainer component and style h1
    Co-authored-by: Cristina Peña <cristinalpena@gmail.com>

---
+++ src/components/EventsContainer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="events-container">
+    <h1>coming up:</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "events-container",
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+h1 {
+  color: #EF476F;
+  font-size: 3.5em;
+  margin-top: .18em;
+}
+
+.events-container {
+  height: 100vh;
+  width: 78%;
+  display: flex;
+  justify-content: center;
+
+}
+</style>
