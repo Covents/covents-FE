f6b93d965914e4458fdfbda8f532e0e84415586b
Cristina Pena <cristinalpena@gmail.com>
2020-05-26 15:10:43 -0600

    Swap out calendar dependency for simpler option
    Co-authored-by: Megan Venetianer <megan.venetianer@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -45,39 +45,4 @@ export default {
     flex-flow: row nowrap;
   }
 
-/* Calendar Styling */
-  
-  .vuecal__menu, .vuecal__cell-events-count {
-    background-color: #ffffff;
-  }
-  .vuecal--green-theme .vuecal__title-bar {
-    background-color: #9ed7ea;
-    border-radius: 10px 10px 0 0;
-  }
-  .vuecal__title-bar button {
-    font-size: 20px;
-    text-transform: uppercase;
-  }
-
-
-  .vuecal--green-theme .vuecal__cell--today, 
-  .vuecal--green-theme .vuecal__cell--current {
-    background-color: #EF476F;
-  }
-  .vuecal--green-theme .vuecal:not(.vuecal--day-view) .vuecal__cell--selected {
-    background-color: #EF476F;
-  }
-  .vuecal--green-theme .vuecal__cell--selected:before {
-    border-color: #EF476F;
-  }
-
-  .vuecal--green-theme .vuecal__cell--highlighted:not(.vuecal__cell--has-splits),
-  .vuecal--green-theme .vuecal__cell-split--highlighted {
-    background-color: #EF476F;
-  }
-  .vuecal--green-theme .vuecal__arrow.vuecal__arrow--highlighted,
-  .vuecal--green-theme .vuecal__view-btn.vuecal__view-btn--highlighted {
-    background-color: #EF476F;
-  }
-
 </style>
---
+++ src/components/CalendarContainer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="calendar-container">
+        <Calendar />
+    </div>
+</template>
+
+<script>
+import Calendar from 'v-calendar/lib/components/calendar.umd';
+
+export default {
+    name: "calendar-container",
+    components: {
+        Calendar,
+    }
+}
+</script>
+
+<style scoped>
+
+ .calendar-container {
+    border-radius: 10px;
+    font-family: 'Roboto Condensed', sans-serif;
+    font-size: 14px;
+ }
+
+</style>
