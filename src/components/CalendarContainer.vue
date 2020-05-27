d9b4e63d7eb7130bb4b83bf802fd76b4ee19e876
Cristina Pena <cristinalpena@gmail.com>
2020-05-27 09:02:35 -0600

    Fix indentations

--- src/components/CalendarContainer.vue
+++ src/components/CalendarContainer.vue
@@ -1,60 +1,58 @@
 <template>
-    <div class="calendar-container">
-        <v-date-picker
-            mode='single'
-            color='blue'
-            v-model='selectedDate'
-            :theme-styles='themeStyles'
-            is-inline>
-        </v-date-picker>
-    </div>
+  <div class="calendar-container">
+    <v-date-picker
+      mode="single"
+      color="blue"
+      v-model="selectedDate"
+      :theme-styles="themeStyles"
+      is-inline
+    >
+    </v-date-picker>
+  </div>
 </template>
 
 <script>
-
 export default {
-    name: "calendar-container",
-    data () {
+  name: "calendar-container",
+  data() {
     return {
       selectedDate: null,
       themeStyles: {
         wrapper: {
-          border: '1',
+          border: "1",
         },
         header: {
-          color: '#fafafa',
-          backgroundColor: '#f142f4',
-          borderColor: '#404c59',
-          borderWidth: '1px 1px 0 1px',
+          color: "#fafafa",
+          backgroundColor: "#f142f4",
+          borderColor: "#404c59",
+          borderWidth: "1px 1px 0 1px",
         },
         headerVerticalDivider: {
-          borderLeft: '1px solid #404c59',
+          borderLeft: "1px solid #404c59",
         },
         weekdays: {
-          color: '#ffffff',
-          backgroundColor: '#f142f4',
-          borderColor: '#ff0098',
-          borderWidth: '0 1px',
-          padding: '5px 0 10px 0',
+          color: "#ffffff",
+          backgroundColor: "#f142f4",
+          borderColor: "#ff0098",
+          borderWidth: "0 1px",
+          padding: "5px 0 10px 0",
         },
         weekdaysVerticalDivider: {
-          borderLeft: '1px solid #404c59',
+          borderLeft: "1px solid #404c59",
         },
         weeks: {
-          border: '1px solid #dadada',
+          border: "1px solid #dadada",
         },
       },
     };
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-
- .calendar-container {
-    border-radius: 10px;
-    font-family: 'Roboto Condensed', sans-serif;
-    font-size: 14px;
- }
-
+.calendar-container {
+  border-radius: 10px;
+  font-family: "Roboto Condensed", sans-serif;
+  font-size: 14px;
+}
 </style>
